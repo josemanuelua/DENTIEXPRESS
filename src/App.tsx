895688ff3015fa51e916ccbc0b707a9c3aef7bc2
Jose Manuel <jmsg00702@gmail.com>
2025-06-03 12:06:01 +0200

    cambio a HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Home from './components/Home/Home';
